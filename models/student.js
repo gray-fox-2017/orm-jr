3c6a922596f142d29278d507e326eb0549bc141e
raynormw <tirtawiryaputra@yahoo.com>
2017-05-10 19:55:56 +0700

    all features done

--- models/student.js
+++ models/student.js
@@ -1,18 +1,19 @@
 "use strict"
 
 class Student {
-  constructor(firstname, lastname, cohort_id, id) {
+  constructor(id, firstname, lastname, cohort_id) {
+    this.id = id;
     this.firstname = firstname;
     this.lastname = lastname;
     this.cohort_id = cohort_id;
-    this.id = id;
   }
 
   static create(db, obj) {
     db.serialize(() => {
-     db.run(`INSERT INTO students (firstname, lastname, cohort_id) VALUES ('${obj.firstname}', '${obj.lastname}', '${obj.cohort_id}');`,
+     db.run(`INSERT INTO students (id, firstname, lastname, cohort_id) VALUES ('${obj.id}', '${obj.firstname}', '${obj.lastname}', '${obj.cohort_id}');`,
      (err) => err ? console.log(err) : console.log('STUDENT ADDED'));
    });
+   return "Process:";
   }
 
   static update(db, obj) {
@@ -20,24 +21,47 @@ class Student {
       db.run(`UPDATE students SET firstname = '${obj.firstname}', lastname = '${obj.lastname}', cohort_id = '${obj.cohort_id}' WHERE id = ${obj.id};`,
       (err) => err ? console.log(err) : console.log('STUDENT UPDATED'));
     });
+    return "Process:";
   }
 
   static delete(db, id) {
     db.serialize(() => {
       db.run(`DELETE FROM students WHERE id = ${id};`, (err) =>  err ? console.log(err) : console.log('STUDENT DELETED'));
     });
+    return "Process:";
   }
 
   static findById(db, id) {
     db.each(`SELECT * FROM students WHERE id = '${id}';`, (err,data) => err ? console.log(err) : console.log(data));
+    return "Process:";
   }
 
   static findAll(db, callback) {
     db.all('SELECT * FROM students', callback);
+    return "Process:";
+  }
+
+  static findLimit(db, obj, callback) {
+    db.all(`SELECT * FROM students LIMIT ${obj.limit} OFFSET ${obj.offset}`, callback);
+    return "Process:";
   }
 
   static where(db, condition, callback) {
     db.all(`SELECT * FROM students WHERE ${condition}`, callback);
+    return "Process:";
+  }
+
+  static findOrCreate(db, obj) {
+    let Student = this;
+    db.all(`SELECT * FROM students WHERE id = ${obj.id} AND firstname = '${obj.firstname}' AND lastname = '${obj.lastname}' AND cohort_id = ${obj.cohort_id}`, function(err, rows) {
+      if(err) {
+        console.log(err);
+      } else if (rows.length === 0) {
+        Student.create(db, obj);
+      } else {
+        console.log(rows);
+      }
+    });
   }
 }
 
